01fb6a41178e2d33fdebe4c9c801d3bd8caaf55e
Bogdan Kirillov <bakirillov1488@gmail.com>
2017-07-10 22:32:29 +0300

    initial commit

---
+++ cnportraits.py
@@ -0,0 +1,117 @@
+"""
+This is an implementation of an algorithm proposed in
+"Portraits of complex networks" by Bagrow, Bollt, Scufca and ben-Abraham.
+Arxiv link of their paper: https://arxiv.org/abs/cond-mat/0703470v2
+
+Author of implementation:
+Bogdan Kirillov (8k1r1ll0v@gmail.com, bakirillov@edu.hse.ru)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+import argparse
+import numpy as np
+import igraph as ig
+import matplotlib.pyplot as plt
+
+
+class Portrait():
+    
+    def __init__(self, graph):
+        self.g = graph
+
+    @staticmethod
+    def b2p(m):
+        """Convert B-matrix to viewable picture"""
+        y = m.shape[0]
+        x = m.shape[1]
+        lg = np.log(m+1)#+1 to get rid of infinities in log
+        n = int(x/y)
+        return(
+            np.array([[lg[a,:]]*n for a in range(lg.shape[0])]).flatten().reshape((n*y, x))
+        )
+        
+    def compute(self):
+        """Compute portrait matrix (B)"""
+        d = self.g.diameter()#Network diameter
+        n = self.g.vcount()#Number of nodes
+        self.B = np.zeros((d+1, n))
+        maxPath = 1
+        for s in range(n):
+            nsPrev = 0
+            maxPath = 1
+            for l in range(d):
+                ns = self.g.neighborhood_size(s, order=l)
+                if l > 0:
+                    #Since l-shell is a difference between
+                    #the neighborhood of order at most l and
+                    #of order at most l-1
+                    ns -= nsPrev
+                am = np.argmax(self.B[l,])
+                #maximal path is l that is bigger than any other nonzero l
+                maxPath = am if am > maxPath else maxPath
+                nsPrev = ns
+                self.B[l, ns] += 1
+        self.B = self.B[:maxPath+1,:]#get the meaningful part of the matrix
+        
+    def draw(self):
+        """Draw the portrait"""
+        k = Portrait.b2p(self.B)
+        self.I = plt.imshow(
+            k, interpolation="nearest"
+        )
+
+    def saveMatrix(self, fn):
+        """Save the matrix B as B.npy"""
+        np.save(fn, self.B)
+
+    def savePicture(self, fn):
+        """Save the portrait as picture"""
+        plt.savefig(fn)
+        
+def startDrawing(args):
+    """Portrait drawing routine"""
+    P = Portrait(ig.load(args.graphF))
+    P.compute()
+    if args.modeV == "matrix":
+        P.saveMatrix(args.outputF)
+    elif args.modeV == "picture":
+        P.draw()
+        P.savePicture(args.outputF)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "graphF",
+        metavar="Graph",
+        action="store",
+        help="Input file"
+    )
+    parser.add_argument(
+        "outputF",
+        metavar="Portrait",
+        action="store",
+        help="Output picture or matrix"
+    )
+    parser.add_argument(
+        "modeV",
+        metavar="Mode",
+        action="store",
+        help="Output mode",
+        choices=["matrix", "picture"]
+    )
+    parser.set_defaults(func=startDrawing)
+    args = parser.parse_args()
+    startDrawing(args)
